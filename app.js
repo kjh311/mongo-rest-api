7fab8e29d9fcfc2afd73d38a45854e0c05269e33
kjh311 <kjh311@gmail.com>
2017-03-29 08:11:07 -0700

    done

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongojs = require('mongojs');
+const db = mongojs('cateloge', ['products']);
 
 const app = express();
 
 const port = 3000;
 
 app.use(bodyParser.json());
+
+
+// Make a public API
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.get('/', (req, res, next) => {
   res.send('Please use /api/products');
@@ -15,27 +24,64 @@ app.get('/', (req, res, next) => {
 
 // Fetch all products
 app.get('/api/products', (req, res, next) =>{
-  res.send('List Products');
+  db.products.find((err, docs) => {
+    if(err){
+      res.send(err);
+    }
+    console.log('Products found...');
+    res.json(docs);
+  });
 });
 
 // Fetch single products
 app.get('/api/products/:id', (req,res,next) =>{
-  res.send('Fetch product'+req.params.id);
+  db.products.findOne({_id: mongojs.ObjectId(req.params.id)}, (err, doc) => {
+    if(err){
+      res.send(err);
+    }
+    console.log('Product found...');
+    res.json(doc);
+  });
 });
 
 // Add Product
 app.post('/api/products', (req,res,next) =>{
-  res.send('Add product');
+  db.products.insert(req.body, (err, doc) => {
+    if(err){
+      res.send(err);
+    }
+    console.log('Adding Product');
+    res.json(doc);
+  });
 });
 
 // Update Product
 app.put('/api/products/:id', (req,res,next) =>{
-  res.send('Update product'+req.params.id);
+  db.products.findAndModify({query: {_id: mongojs.ObjectId(req.params.id)},
+    update:{
+      $set:{
+        name: req.body.name,
+        category: req.body.category,
+        details: req.body.details
+      }},
+      new: true }, (err, doc) => {
+        if(err){
+          res.send(err);
+        }
+        console.log('Updating Product');
+        res.json(doc);
+      })
 });
 
 // Delete Product
 app.delete('/api/products/:id', (req,res,next) =>{
-  res.send('Delete product'+req.params.id);
+  db.products.remove({_id: mongojs.ObjectId(req.params.id)}, (err, doc) => {
+  if(err){
+          res.send(err);
+        }
+        console.log('Removing Product...');
+        res.json(doc);
+      });
 });
 
 app.listen(port, () =>{
